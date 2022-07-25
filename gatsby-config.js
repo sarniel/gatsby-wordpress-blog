976fb87aac05240845e63afc7c50e24552ecb22e
sarniel <sarnielmorgia@gmail.com>
2022-07-25 18:22:22 +1000

    change api graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,7 @@ module.exports = {
       resolve: `gatsby-source-wordpress`,
       options: {
         // the only required plugin option for WordPress is the GraphQL url.
-        url:
-          process.env.WPGRAPHQL_URL ||
-          `https://wp.siteinspectionreporting.com.au/graphql`,
+        url: process.env.WPGRAPHQL_URL || `https://archsdigest.com/graphql`,
       },
     },
 
